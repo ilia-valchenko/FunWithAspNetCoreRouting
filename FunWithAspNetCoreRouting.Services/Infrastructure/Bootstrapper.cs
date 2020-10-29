21a73105db6ef3d9fa958e60fc75b4d14e43cd83
Ilya Valchanka <Ilya.Valchanka@thomsonreuters.com>
2020-10-29 10:43:37 +0300

    Created MVC application skeleton

---
+++ FunWithAspNetCoreRouting.Services/Infrastructure/Bootstrapper.cs
@@ -0,0 +1,14 @@
+﻿using FunWithAspNetCoreRouting.Services.Services;
+using FunWithAspNetCoreRouting.Services.Services.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FunWithAspNetCoreRouting.Services.Infrastructure
+{
+    public static class Bootstrapper
+    {
+        public static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<IPersonService, PersonService>();
+        }
+    }
+}
---
+++ FunWithAspNetCoreRouting.Services/Services/PersonService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using FunWithAspNetCoreRouting.Domain.Entities;
+using FunWithAspNetCoreRouting.Services.Services.Interfaces;
+
+namespace FunWithAspNetCoreRouting.Services.Services
+{
+    public class PersonService : IPersonService
+    {
+        private readonly IEnumerable<Person> persons = new[]
+        {
+            new Person
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "Ilya",
+                LastName = "Valchanka",
+                Age = 25
+            },
+            new Person
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "John",
+                LastName = "Doe",
+                Age = 30
+            },
+            new Person
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "John",
+                LastName = "Smith",
+                Age = 31
+            }
+        };
+
+        public async Task<IEnumerable<Person>> GetAsync(CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return await Task.Run(() => persons, cancellationToken);
+        }
+
+        public async Task<IEnumerable<Person>> GetAsync(string firstName, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return string.IsNullOrWhiteSpace(firstName)
+                ? await Task.Run(() => this.GetAsync(cancellationToken))
+                : await Task.Run(() => persons.Where(p => string.Equals(p.FirstName, firstName, StringComparison.OrdinalIgnoreCase)), cancellationToken);
+        }
+
+        public async Task<Person> GetAsync(Guid id, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return await Task.Run(
+                () => persons.FirstOrDefault(p => p.Id == id),
+                cancellationToken);
+        }
+    }
+}
